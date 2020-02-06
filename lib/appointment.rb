f04b664779ea770f65e2cbf6514df84aa88d5915
Micah Banschick <micha.ban@gmail.com>
2020-02-06 21:17:28 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   
   @@all = []
   
-  def initialize(date, doctor, patient)
+  def initialize(date, patient, patient)
     @date = date 
     @doctor = doctor 
     @patient = patient 
